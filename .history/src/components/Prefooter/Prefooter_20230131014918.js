0e9c61aa8f681a88bc55a2ca5dddf75a5dfceca5
Sarah <sarahnnanyelugo@gmail.com>
2023-01-31 02:11:36 +0100

    screen resolution

---
+++ .history/src/components/Prefooter/Prefooter_20230131014918.js
@@ -0,0 +1,32 @@
+import Grad from "../../assets/grad.png";
+import Img1 from "../../assets/basketball.png";
+import { Link } from "react-router-dom";
+import "./_prefooter.scss";
+export const Prefoooter = () => {
+  return (
+    <>
+      {" "}
+      <div className="containa col-md-12 ">
+        <center>
+          <div className="col-md-3 flexy">
+            {" "}
+            <h1>
+              See Why We’re the Best Private Christian School to Consider.
+            </h1>
+          </div>
+        </center>
+        <div className="col-md-12 flexy ">
+          {/* <div className="col-md-3">&nbsp;</div> */}
+          <div className="col-md-10 flexy">
+            <center>
+              {" "}
+              <button>REQUEST INFORMATION</button>
+              <button>SCHEDULE A TOUR</button>
+              <button>APPLY NOW</button>
+            </center>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
